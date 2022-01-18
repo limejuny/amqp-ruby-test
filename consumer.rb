9a509a756f9fcfc9f2158c219610141e9a02268a
임준영 <limejuny@gmail.com>
2022-01-18 23:44:37 +0900

    consumer가 pid를 출력
    Detail
    ======
     - 여러 worker를 동작시키고 구분하기 위해서 pid를 출력

--- consumer.rb
+++ consumer.rb
@@ -5,13 +5,10 @@ class Counter
     @count = 0
   end
 
-  def plus()
+  def pid_and_count()
     @count += 1
-  end
-
-  def print()
     50000000.times { }
-    puts @count
+    print "PID[#{Process.pid}][#{@count}]\t"
   end
 end
 
@@ -20,15 +17,16 @@ EventMachine.run do
                             :user => ENV["RABBITMQ_USER"],
                             :pass => ENV["RABBITMQ_PASS"],
                             :vhost => ENV["RABBITMQ_VHOST"])
-  puts "Connecting to RabbitMQ. Running #{AMQP::VERSION} version of the gem..."
+  # puts "Connecting to RabbitMQ. Running #{AMQP::VERSION} version of the gem..."
+  puts "PID[#{Process.pid}]: Connecting to RabbitMQ."
   c = Counter.new
 
   ch = AMQP::Channel.new(connection)
   q = ch.queue(ENV["RABBITMQ_QUEUE"], :auto_delete => false)
 
   q.subscribe(:block => false) do |metadata, payload|
-    puts "\nReceived a message: #{payload}"
-    c.plus
-    c.print
+    puts "\nPID[#{Process.pid}]\tReceived a message: #{payload}"
+    c.pid_and_count
+    puts "100 / #{payload} = #{100 / payload.to_i}"
   end
 end
